9c4766cf9b87586432d8574a9dcf29fdb47b4a3b
Alex Norman <xnor@cycling74.com>
2024-02-28 15:36:59 -0800

    using pidfile because name doesn't seem to work

--- config/rnbomovecontrol-init.d
+++ config/rnbomovecontrol-init.d
@@ -7,13 +7,13 @@
 # Attempt to start rnbomovecontrol
 
 cmd=/data/UserData/rnbo/rnbomovecontrol
-name=rnbomovecontrol
+pidfile=/tmp/rnbomovecontrol.pid
 
 if [ "$1" = "start" ]; then
     if [ -e $cmd ]; then
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/UserData/jack2/lib/
-        start-stop-daemon --start --name $name -c ableton -b $cmd
+        start-stop-daemon --start --pidfile $pidfile --make-pidfile -c ableton -b -x $cmd
     fi
 elif [ "$1" = "stop" ]; then
-    start-stop-daemon --stop --name $name
+    start-stop-daemon --stop --pidfile $pidfile
 fi
